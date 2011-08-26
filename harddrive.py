0d0e9906f625bea5584531159ff021530c49a59b
Levi Gross <levi@levigross.com>
2011-08-26 05:13:35 -0400

    Run forever

--- harddrive.py
+++ harddrive.py
@@ -4,19 +4,22 @@ __version__ = 0.1
 
 from gevent.monkey import patch_all; patch_all()
 import gevent
-import os,sys,string,random
+import string,random,os
 
 def fill_io_buffer(num_files):
-    files = []
-    for i in xrange(num_files):
-        files.append(open(''.join((random.choice(string.ascii_letters) for i in xrange(10))),'w'))
-    events = []
-    for i in files:
-        events.append(gevent.spawn(_fill_file,i))
-        events.append(gevent.spawn(_read_file,i))
-    for i in events:
-        i.join()
-
+    while True:
+        files = []
+        for i in xrange(num_files):
+            files.append(open(''.join((random.choice(string.ascii_letters) for i in xrange(10))),'w'))
+        events = []
+        for i in files:
+            events.append(gevent.spawn(_fill_file,i))
+            events.append(gevent.spawn(_read_file,i))
+        for i in events:
+            i.join()
+        for i in files:
+            os.remove(i.name)
+            
 
 def _fill_file(fd):
     while True:
